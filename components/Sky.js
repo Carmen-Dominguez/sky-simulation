6c45e640afc3dcb470fd1452d664506037577425
Carmen Dominguez <carmengdominguez94@gmail.com>
2025-05-19 14:01:18 +0200

    feat: sky changes

--- components/Sky.js
+++ components/Sky.js
@@ -9,8 +9,10 @@ class Sky {
     createSky() {
         const skyGeometry = new THREE.SphereGeometry(500, 32, 32);
         const skyMaterial = new THREE.MeshBasicMaterial({
-            color: 0x87CEEB,
-            side: THREE.BackSide
+            color: 0x87CEEB, // color doesn't matter now
+            side: THREE.BackSide,
+            transparent: true,
+            opacity: 0
         });
         this.sky = new THREE.Mesh(skyGeometry, skyMaterial);
         this.scene.add(this.sky);
